a534edd71994e73fc3bb71688d8a97150d872ea8
kelvin-vd-gaag <kelvinvandergaag@protonmail.com>
2021-07-11 16:16:33 +0200

    fixed width scaling issue on mobile

--- javascript/main.js
+++ javascript/main.js
@@ -1,11 +1,4 @@
-// $(document).ready(function(){
-//     $('#nav-icon4').click(function(){
-//         $(this).toggleClass('open');
-//     });
-// });
-
-
-document.getElementById("nav-icon4").addEventListener("click", event =>{
-    document.getElementById("nav-icon4").classList.toggle("open");
+document.getElementById("hamburger-menu").addEventListener("click", event =>{
+    document.getElementById("hamburger-menu").classList.toggle("open");
     document.getElementById("mobile-menu").classList.toggle("active");
 })
