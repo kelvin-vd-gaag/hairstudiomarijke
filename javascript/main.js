01aca278f3c4ce055c69efec0773b924ab3dcfa9
kelvin-vd-gaag <kelvinvandergaag@protonmail.com>
2021-07-15 20:23:50 +0200

    added overlay when opening mobile menu

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,5 @@
 document.getElementById("hamburger-menu").addEventListener("click", event =>{
     document.getElementById("hamburger-menu").classList.toggle("open");
     document.getElementById("mobile-menu").classList.toggle("active");
+    document.getElementById("overlay").classList.toggle('overlay__active')
 })
