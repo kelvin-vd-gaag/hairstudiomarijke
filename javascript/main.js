fc0335088e6104cddc7ac0801a9856aa856291b6
kelvin-vd-gaag <kelvinvandergaag@protonmail.com>
2021-07-15 20:27:26 +0200

    fixed overlay styling issue and cleaned up the javascript

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,13 @@
 document.getElementById("hamburger-menu").addEventListener("click", event =>{
+   toggle_menu();
+})
+
+document.getElementById("overlay").addEventListener("click", event=>{
+   toggle_menu();
+})
+
+function toggle_menu(){
+    document.getElementById("overlay").classList.toggle("overlay__active");
     document.getElementById("hamburger-menu").classList.toggle("open");
     document.getElementById("mobile-menu").classList.toggle("active");
-    document.getElementById("overlay").classList.toggle('overlay__active')
-})
+}
