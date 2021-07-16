959d266652abdc9e1af470339eeff2f9e4d00937
kelvin-vd-gaag <kelvinvandergaag@protonmail.com>
2021-07-16 02:17:54 +0200

    added dynamic opening hours depening on the day

--- javascript/main.js
+++ javascript/main.js
@@ -11,3 +11,27 @@ function toggle_menu(){
     document.getElementById("hamburger-menu").classList.toggle("open");
     document.getElementById("mobile-menu").classList.toggle("active");
 }
+
+var date = new Date();
+//Get the day of the week in a number
+var number = date.getDay();
+
+let opening_hours =  [
+    //Monday
+    'Vandaag open van: 09:00 tot 18:00',
+    //Tuesday
+    'Vandaag open van: 09:00 tot 18:00',
+    //Wednesday
+    'Vandaag open van: 09:00 tot 18:00',
+    //Thursday
+    'Vandaag open van: 09:00 tot 18:00',
+    //Friday
+    'Vandaag open van: 09:00 tot 18:00',
+    //Saturday
+    'Vandaag gesloten',
+    //Sunday
+    'Vandaag gesloten',
+]
+
+//opening_hours[number - 1] as the week starts at day 1 but array counting starts with 0
+document.getElementById("opening-hours").innerHTML += opening_hours[number - 1]
